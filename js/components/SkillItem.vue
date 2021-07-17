a46982bd7a37357f68eab5764d6824c904b60f2c
aldoBangun <punk.aldo68@gmail.com>
2021-07-17 18:18:38 +0800

    add SkillList.vue and SkillItem.vue to js/components/

---
+++ js/components/SkillItem.vue
@@ -0,0 +1,18 @@
+<template>
+   <li class="flex items-center gap-4">
+      <span class="text-sm text-brand-light"> ▶ </span> <span> {{ skill }} </span>
+   </li>
+</template>
+
+<script>
+
+export default {
+   props: {
+      skill: {
+         type: String,
+         required: true
+      }
+   }
+}
+
+</script>
---
+++ js/components/SkillList.vue
@@ -0,0 +1,21 @@
+<template>
+   <ul class="text-gray-500 grid grid-cols-2 gap-4">
+      <SkillItem v-for="skill in skills" :key="skill" :skill="skill" />
+   </ul>
+</template>
+
+<script>
+import SkillItem from './SkillItem.vue';
+
+export default {
+   components: {
+      SkillItem
+   },
+   data() {
+      return {
+         skills: ['HTML5', 'CSS3', 'Javascript / ES6+', 'Tailwind CSS', 'Vue.js']
+      }
+   },
+}
+
+</script>
